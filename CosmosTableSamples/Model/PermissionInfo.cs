3f5efb273e2f01a089cd35cc56f5b78e9e846809
Basu, Avinava <avinavabas@youforce.net>
2019-07-02 18:34:44 +0530

    Add project files.

---
+++ CosmosTableSamples/Model/PermissionInfo.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CosmosTableSamples.Model
+{
+    public class PermissionInfo : TableEntity
+    {
+
+    }
+}
---
+++ CosmosTableSamples/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CosmosTableSamples
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Azure Cosmos Table Samples");
+            //BasicSamples basicSamples = new BasicSamples();
+            //basicSamples.RunSamples().Wait();
+
+            AdvancedSamples advancedSamples = new AdvancedSamples();
+
+            advancedSamples.RunSamples("UserDetails").Wait();
+            advancedSamples.RunSamples("UserPermission").Wait();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
+            Console.Read();
+        }
+    }
+}
